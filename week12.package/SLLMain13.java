0b44df55bf73146450225027e5ece4fe7bd94d6e
luciakusumaw <itsmelucia27@gmail.com>
2026-05-13 20:14:35 +0700

    Modify Exp 1

--- week12.package/SLLMain13.java
+++ week12.package/SLLMain13.java
@@ -1,19 +1,32 @@
+import java.util.Scanner;
+
 public class SLLMain13 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList13 sll = new SingleLinkedList13();
+        System.out.print("Input the number of students to add: ");
+        int total = sc.nextInt();
+        sc.nextLine(); 
 
-        Student13 std1 = new Student13("001", "Student 1", "TI-1I", 3.89);
-        Student13 std2 = new Student13("002", "Student 2", "TI-1I", 3.45);
-        Student13 std3 = new Student13("003", "Student 3", "TI-1I", 3.20);
-        Student13 std4 = new Student13("004", "Student 4", "TI-1I", 3.00);
+        for (int i = 0; i < total; i++) {
+            System.out.println("--- Input Student " + (i + 1) + " ---");
+            System.out.print("NIM           : ");
+            String nim = sc.nextLine();
+            System.out.print("Name          : ");
+            String name = sc.nextLine();
+            System.out.print("Class Name    : ");
+            String className = sc.nextLine();
+            System.out.print("GPA           : ");
+            double gpa = sc.nextDouble();
+            sc.nextLine(); 
 
-        sll.print();
-        sll.addFirst(std4);
-        sll.print();
-        sll.addLast(std1);
-        sll.print();
-        sll.insertAfter(std3, "Student 4");
-        sll.insertAt(2, std2);
+           
+            Student13 std = new Student13(nim, name, className, gpa);
+            
+            sll.addLast(std);
+        }
+
+        System.out.println("\nFinal Data in Linked List:");
         sll.print();
     }
 }
